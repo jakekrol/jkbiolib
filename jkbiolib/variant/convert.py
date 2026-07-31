07197886a45ce32b90e40979c2fc52d930fa664b
jakekrol <jdkrol351@gmail.com>
2026-07-31 10:36:49 -0600

    return total samples in variant.vcf.count_alt_samples() for pop freq norm; rename modules in variant

--- jkbiolib/variant/convert.py
+++ jkbiolib/variant/convert.py
@@ -1,4 +1,5 @@
 from cyvcf2 import VCF
+import subprocess
 
 def vcf2stix_queries(path_vcf, path_out, out_header=False):
     VALID_SVTYPES = {'DEL', 'DUP', 'INV', 'INS', 'BND'}
@@ -44,3 +45,40 @@ def vcf2stix_queries(path_vcf, path_out, out_header=False):
             f.write(f"{id}\t{variant.CHROM}\t{left_start}\t{left_end}\t{right_start}\t{right_end}\t{svtype}\n")
             
                     
+def vcf2bed(path_vcf, path_out, out_header=False):
+    cmd = [
+        "bcftools",
+        "query",
+        "-f",
+        "%CHROM\t%POS0\t%END\t%ID\n",
+        path_vcf
+    ]
+    if out_header:
+        with open(path_out, "w") as f:
+            f.write("CHROM\tSTART\tEND\tID\n")
+    with open(path_out, "a") as f:
+        subprocess.run(cmd, stdout=f)
+    # VALID_SVTYPES = {'DEL', 'DUP', 'INV', 'INS', 'BND'}
+    # vcf = VCF(path_vcf)
+    # with open(path_out, 'w') as f:
+    #     if out_header:
+    #         f.write("CHROM\tSTART\tEND\tID\tSVTYPE\n")
+    #     for i, variant in enumerate(vcf):
+    #         id = variant.ID if variant.ID is not None else f"var_{i}"
+    #         ### svtype
+    #         svtype = variant.INFO.get('SVTYPE')
+    #         if svtype not in VALID_SVTYPES:
+    #             print("# warning: skipping variant with unsupported SVTYPE:", svtype)
+    #             continue
+    #         ### coordinates
+    #         start = variant.POS
+    #         # left_start, left_end = position, position
+    #         if svtype != 'INS':
+    #             end = variant.INFO.get('END')
+    #         else:
+    #             svlen = variant.INFO.get('SVLEN')
+    #             if svlen is None:
+    #                 print("# warning: skipping INS variant with missing SVLEN:", id)
+    #                 continue
+    #             end = start + svlen
+    #         f.write(f"{id}\t{variant.CHROM}\t{start}\t{end}\t{id}\t{svtype}\n")
--- jkbiolib/variant/vcf.py
+++ jkbiolib/variant/vcf.py
@@ -1,13 +1,49 @@
 from cyvcf2 import VCF, Writer
+import pandas as pd
 import os
 
-def split_vcf(vcf_in, dir_out):
+def count_alt_samples(vcf_in):
+    assert os.path.exists(vcf_in), f"Input VCF file does not exist: {vcf_in}"
+    vcf = VCF(vcf_in)
+    data =[]
+    for i,v in enumerate(vcf):
+        if i == 0:
+            numsamples = len(v.genotypes)
+        svid = v.ID if v.ID is not None else '.'
+        alt_count = 0
+        for gt in v.genotypes:
+            allele1 = gt[0]
+            allele2 = gt[1]
+            if (allele1 == 1) or (allele2 == 1):
+                alt_count+=1
+        data.append((svid, alt_count))
+    alt_sample_counts = pd.DataFrame(data, columns=['SVID', 'Alt_Sample_Count'])
+    return alt_sample_counts, numsamples
+
+def add_id(vcf_in, vcf_out):
+    assert os.path.exists(vcf_in), f"Input VCF file does not exist: {vcf_in}"
+    vcf = VCF(vcf_in)
+    with Writer(vcf_out, vcf) as w:
+        for i, v in enumerate(vcf):
+            if v.ID is None:
+                v.ID = f"{v.CHROM}_{v.POS}_{i}"
+            w.write_record(v)
+
+def split_vcf(vcf_in, dir_out, delimiter='---'):
     assert os.path.exists(vcf_in), f"Input VCF file does not exist: {vcf_in}"
     assert os.path.isdir(dir_out), f"Output directory does not exist: {dir_out}"
     vcf = VCF(vcf_in)
     for i, v in enumerate(vcf):
-        id = v.ID if v.ID is not None else f"var_{i}"
-        out_path = os.path.join(dir_out, f"{id}.vcf")
-        with Writer(out_path, vcf) as w:
-            w.write_record(v)
+        id = v.ID if v.ID is not None else ''
+        chrom = v.CHROM if v.CHROM is not None else ''
+        start = v.POS if v.POS is not None else ''
+        end = v.INFO.get('END') if v.INFO.get('END') is not None else ''
+        svtype = v.INFO.get('SVTYPE') if v.INFO.get('SVTYPE') is not None else ''
+        outfile = f"{i}{delimiter}{id}{delimiter}{chrom}_{start}_{end}_{svtype}.vcf"
+        out_path = os.path.join(dir_out, outfile)
+        try:
+            with Writer(out_path, vcf) as w:
+                w.write_record(v)
+        except Exception as e:
+            print(f"Warning: Could not write record {i} ({chrom}:{start}). Error: {e}")
         
